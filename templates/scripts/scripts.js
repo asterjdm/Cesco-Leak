886287cfe98b62491ed6cf07c1f1152dae2eb4e8
AsteroidusTv <asteroidus@protonmail.com>
2024-05-05 11:11:22 +0200

    Add new professor popup

---
+++ templates/scripts/scripts.js
@@ -0,0 +1,15 @@
+const imageInput = document.getElementById('uploadPhoto');
+const previewImage = document.getElementById('previewImage');
+
+imageInput.addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            previewImage.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        previewImage.src = '#';
+    }
+});
