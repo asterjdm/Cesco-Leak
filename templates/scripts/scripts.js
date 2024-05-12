0ac730ac3f0fe45cd4bd4ff264d07cf6580ab36f
AsteroidusTv <asteroidus@protonmail.com>
2024-05-12 17:27:45 +0200

    Ratings popup

--- templates/scripts/scripts.js
+++ templates/scripts/scripts.js
@@ -2,6 +2,15 @@ const imageInput = document.getElementById('uploadPhoto');
 const previewImage = document.getElementById('previewImage');
 const standardPopupContentMessages = document.getElementById('standardPopupContentMessages')
 
+const ratingValues = document.querySelectorAll(".value");
+const ratingTexts = document.querySelectorAll(".score-value p");
+
+ratingValues.forEach((value, index) => {
+    value.addEventListener("input", function() {
+        ratingTexts[index].textContent = this.value;
+    });
+});
+
 imageInput.addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (file) {
