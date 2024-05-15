89773e1ee0b0c4ab3d885af1e73dbed56322fab0
JdM <julien@rmbi.ch>
2024-05-15 16:11:25 +0200

    added newTeacher.php

---
+++ api/newTeacher.php
@@ -0,0 +1,12 @@
+<?php
+include_once(dirname(__FILE__) . "/secrets.php");
+
+
+$name = $_POST['name'];
+$msg = "Name: " . $name;
+
+for ($i=0; $i < count(EMAILS); $i++) { 
+    mail(EMAILS[$i], "CESCOLEAKS Teacher request", $msg);
+}
+
+?>
