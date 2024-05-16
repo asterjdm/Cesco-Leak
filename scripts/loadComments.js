1d095f640be1755d0e052ad55962e37c3b91efd2
JdM <julien@rmbi.ch>
2024-05-16 19:05:33 +0200

    unique identifier for comments

--- scripts/loadComments.js
+++ scripts/loadComments.js
@@ -6,7 +6,9 @@ function loadComments(comments) {
   for (let i = 0; i < comments.length; i++) {
     commentsHtml += /*html*/ `
     <div class="message">
-        <p class="user">39273</p>
+        <p class="user">USER#${comments[i].IP.match(/\d/g)
+          .join("")
+          .slice(0, 6)}:</p>
         <p>${comments[i].content}</p>
     </div>
     `;
