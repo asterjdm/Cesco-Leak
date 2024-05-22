1ed46c7e3dc151eea0ebebf6d2eedb2fe7aa7de1
JdM <julien@rmbi.ch>
2024-05-22 16:02:08 +0200

    s

--- api/sendMail.php
+++ api/sendMail.php
@@ -5,10 +5,10 @@ include_once(dirname(__FILE__) . "/utils/getClientIp.php");
 
 $userMsg = $_GET['msg'];
 $clientIP = getClientIp();
-$msg = "Le message du débile en question : $userMsg\n Son IP (on sais jamais) $clientIP";
+$msg = "Le message: $userMsg\n Son IP (on sais jamais) $clientIP";
 
 for ($i = 0; $i < count(EMAILS); $i++) {
-    mail(EMAILS[$i], "CESCOLEAKS: Ya un débile qui nous envoi un message", $msg);
+    mail(EMAILS[$i], "CESCOLEAKS: message", $msg);
 }
 
 if (isset($_GET["return"])) {
